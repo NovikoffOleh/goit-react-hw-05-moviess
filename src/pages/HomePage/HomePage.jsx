dd3c024191b2fcf86b0016b0fd0828b310d0681e
NovikoffOleh <novikoff10000@gmail.com>
2023-02-25 23:51:17 +0200

    correct

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -25,8 +25,8 @@ export default function HomePage() {
   useEffect(() => {
     addBackToTop({
       diameter: 50,
-      backgroundColor: 'rgb(14, 21, 56)',
-      textColor: '#aaaaaa',
+      backgroundColor: '#55504c',
+      textColor: '#f8f5f5',
       innerHTML:
         '<svg viewBox="0 0 24 24"><path d="M4 12l1.41 1.41L11 7.83V20h2V7.83l5.58 5.59L20 12l-8-8-8 8z"/></svg>',
       scrollDuration: 1000,
